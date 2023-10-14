75dc0140999e8d7277922887b1e4d9be11d2ad92
Anastasia <barabanova.aaa@gmail.com>
2023-10-14 21:13:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int amount = 102566;
+
+        int miles = 20;
+
+        System.out.println("Начислено миль:");
+        int bonus = amount / miles;
+
+        System.out.println(bonus);
+
+    }
+}
